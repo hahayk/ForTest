7416135d21389e3653b07b7864618d2a86afb61d
Hayk Haru <hayk.haru@gmail..com>
2016-12-14 01:31:23 +0400

    killKthBit

---
+++ SolutionTest/ForTesting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(killKthBit(37, 3));
+        }
+
+        static int killKthBit(int n, int k)
+        {
+            return n & ~(1 << k-1);
+        }
+
+    }
+}
