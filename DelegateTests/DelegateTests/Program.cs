57ab52055b04aaeda11007671a99273a97e66298
Hayk Harutyunyan <hayk.haru@gmail..com>
2017-01-22 19:01:44 +0400

    Delegate Tests

---
+++ DelegateTests/DelegateTests/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTests
+{
+    public delegate int BinaryOp(int x, int y);
+    public class SimpleMath
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Substract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Simple Delegate Example *****\n");
+
+            BinaryOp b = new BinaryOp(SimpleMath.Add);
+            Console.WriteLine($"10 + 10 is {b(10, 10)}");
+        }
+    }
+}
