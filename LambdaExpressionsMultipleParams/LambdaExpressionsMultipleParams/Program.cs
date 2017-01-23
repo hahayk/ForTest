9574272a14de3f68ff5f515a9a6fa17b8e63830a
Hayk Harutyunyan <hayk.haru@gmail..com>
2017-01-24 00:48:43 +0400

    update

--- LambdaExpressionsMultipleParams/LambdaExpressionsMultipleParams/Program.cs
+++ LambdaExpressionsMultipleParams/LambdaExpressionsMultipleParams/Program.cs
@@ -10,6 +10,36 @@ namespace LambdaExpressionsMultipleParams
     {
         static void Main(string[] args)
         {
+            SimpleMath m = new SimpleMath();
+            m.SetMathHandler((msg, result) =>
+            {
+                Console.WriteLine("Message: {0}, Result: {1}", msg, result);
+            });
+
+            m.Add(10, 10);
+            Console.ReadLine();
+
+            VerySimpleDelegate d = new VerySimpleDelegate(() => { return " Text"; });
+            Console.WriteLine(d());
+        }
+
+        public delegate string VerySimpleDelegate();
+
+
+        public class SimpleMath
+        {
+            public delegate void MathMessage(string msg, int result);
+            private MathMessage mmDelgate;
+            public void SetMathHandler(MathMessage target)
+            {
+                mmDelgate = target;
+            }
+
+            public void Add(int x, int y)
+            {
+                mmDelgate?.Invoke("Adding has completed!", x + y);
+            }
+
         }
     }
 }
