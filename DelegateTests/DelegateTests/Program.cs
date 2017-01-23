d99549152d15f8f8892bb62a3b1300fe179bdbee
Hayk Harutyunyan <hayk.haru@gmail..com>
2017-01-24 00:34:50 +0400

    update

--- DelegateTests/DelegateTests/Program.cs
+++ DelegateTests/DelegateTests/Program.cs
@@ -23,8 +23,8 @@ namespace DelegateTests
             Console.WriteLine("***** Delegates as event enablers *****\n");
 
             Car c1 = new Car("SlugBug", 100, 10);
-            c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
-            c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent2));
+            c1.RegisterWithCarEngine(OnCarEngineEvent);
+            c1.RegisterWithCarEngine(OnCarEngineEvent2);
 
             Console.WriteLine("***** Speeding up *****");
             for (int i = 0; i < 6; i++)
---
+++ LambdaExpression/LambdaExpression/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Lambdas *****\n");
+            TraditionalDelegateSyntax();
+            Console.ReadLine();
+        }
+        static void TraditionalDelegateSyntax()
+        {
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+            Predicate<int> callback = IsEvenNumber;
+            //List<int> evenNumbers = list.FindAll(callback);
+            //            List<int> evenNumbers = list.FindAll(delegate(int i)
+            //{
+            //return (i % 2) == 0;
+            //});
+            List<int> evenNumbers = list.FindAll(i =>
+            {
+                Console.WriteLine("value of i is currently: {0}", i);
+                bool isEven = ((i % 2) == 0);
+                return isEven;
+            });
+
+            Console.WriteLine("Here are your even numbers:");
+            foreach (int evenNumber in evenNumbers)
+            {
+                Console.WriteLine($"{evenNumber}\t");
+            }
+            Console.WriteLine();
+        }
+
+        private static bool IsEvenNumber(int i)
+        {
+            return (i % 2) == 0;
+        }
+
+    }
+}
