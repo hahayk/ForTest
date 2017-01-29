c8d794cef20bbeaa321db5178c9dccf869b6bfe3
Hayk Harutyunyan <hayk.haru@gmail.com>
2017-01-30 01:00:28 +0400

    Problem with GROUP

---
+++ LINQ_Test/LINQ_Test/Books.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Test
+{
+    class Books
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+
+
+        public Books(string name, int price, string genre, int year)
+        {
+            Name = name;
+            Price = price;
+            Genre = genre;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Price: {Price}, Genre: {Genre}, Year: {Year}";
+        }
+
+    }
+}
---
+++ LINQ_Test/LINQ_Test/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List < Books > bks = new List<Books>();
+            bks.Add(new Books("Book1", 100, "Genre1", 1999 ));
+            bks.Add(new Books("Book2", 100, "Genre2", 2016));
+            bks.Add(new Books("Book3", 23300, "Genre3", 1999 ));
+            bks.Add(new Books("Book4", 300, "Genre2", 2017 ));
+            bks.Add(new Books("Book5", 45000, "Genre2", 1555 ));
+            bks.Add(new Books("Book5", 5400, "Genre2", 1555 ));
+            bks.Add(new Books("Book5", 45000, "Genre2", 1555 ));
+
+            //get books which genre contains 2
+            var bookByGenre = from book in bks
+                              where book.Genre.Contains("2")
+                              select book;
+            foreach (var item in bookByGenre)
+            {
+                //Console.WriteLine(item);
+            }
+
+            //get sum of books that are published after 1999
+            var bookPriceByYear = (from book in bks
+                                   where book.Year > 1999
+                                   select book.Price).Sum();
+            //Console.WriteLine($"Summary Price is: {bookPriceByYear}");
+
+            //show books price summary by published year
+            var bookGroupBy = from book in bks
+                               group book.Price by book.Name into bPr
+                               select new { Price = bPr.Key, NAME = bPr.ToString()};
+
+            foreach (var item in bookGroupBy)
+            {   
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
