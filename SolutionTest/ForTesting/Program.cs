4f6b84388c5b733faa6da9203bdb848cd5ccd950
Hayk Harutyunyan <hayk.haru@gmail.com>
2017-02-27 23:37:07 +0400

    HashSet

--- SolutionTest/ForTesting/Program.cs
+++ SolutionTest/ForTesting/Program.cs
@@ -12,50 +12,39 @@ namespace Translate
     {
         static void Main(string[] args)
         {
-            HashSet<int> t = new HashSet<int>();
+            HashSet<int> evenNumbers = new HashSet<int>();
+            HashSet<int> oddNumbers = new HashSet<int>();
 
-            try
+            for (int i = 0; i < 5; i++)
             {
-                string[] tokens_n = Console.ReadLine().Split(' ');
-                //number of array of rotate
-                int n = Convert.ToInt32(tokens_n[0]);
-                //shift k time
-                int k = Convert.ToInt32(tokens_n[1]);
-                //count of returned indice
-                int q = Convert.ToInt32(tokens_n[2]);
-
-                string[] a_temp = Console.ReadLine().Split(' ');
-                //Array to rotate
-                int[] a = Array.ConvertAll(a_temp, Int32.Parse);
-                if (a.Length != n)
-                {
-                    return;
-                }
-
-                var arr = a.ToList();
-                var temp = 0;
-                for (int i = 0; i < k; i++)
-                {
-                    temp = arr.Last();
-                    arr.RemoveAt(arr.Count - 1);
-                    arr.Insert(0, temp);
-
-                }
-                
-                //indice to return from array
-                for (int a0 = 0; a0 < q; a0++)
-                {
-                    int m = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine(a[m]);
-                }
-
+                evenNumbers.Add(i * 2);
+                oddNumbers.Add((i * 2) + 1);
             }
 
-            catch (Exception e)
-            {
+            Console.WriteLine($"evenNumbers containg {evenNumbers.Count} elements");
+            DisplaySet(evenNumbers);
 
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine($"evenNumbers containg {evenNumbers.Count} elements");
+            DisplaySet(oddNumbers);
+
+
+            HashSet<int> numbers = new HashSet<int>();
+            Console.WriteLine("numbers UnionWith oddNumbers...");
+            numbers.UnionWith(oddNumbers);
+
+            Console.Write("numbers contains {0} elements: ", numbers.Count);
+            DisplaySet(numbers);
         } 
+
+        private static void DisplaySet(HashSet<int> set)
+        {
+            Console.Write("{");
+            foreach (var item in set)
+            {
+                Console.Write($"{item}");
+            }
+
+            Console.WriteLine("}");
+        }
     }
 }
