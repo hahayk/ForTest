d0c66ba41bcf2e0b58159c1754c0c8d6b4ca64ee
Hayk Harutyunyan <hayk.haru@gmail.com>
2017-03-02 00:10:57 +0400

    Final 1

--- Sherlock and Array/Sherlock and Array/Program.cs
+++ Sherlock and Array/Sherlock and Array/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sherlock_and_Array
 {
@@ -11,34 +8,68 @@ namespace Sherlock_and_Array
     {
         static void Main(string[] args)
         {
-            List<int> arr = new List<int>() { 1, 2, 3, 3 };
+            //test case count
+            int testCaseCnt = Convert.ToInt32(Console.ReadLine());
+            List<int> array = new List<int>();
+
+            for (int i = 0; i < testCaseCnt; i++)
+            {
+                //remove all elements from array
+                array.Clear();
+
+                //get array items from Console
+                int InputArraySize = Convert.ToInt32(Console.ReadLine());
+                //for (int j = 0; j < InputArraySize; j++)
+                {
+                    var str = Console.ReadLine();
+                    string curStr = string.Empty;
+                    int curInd = 0;
+
+                    for (int k = 0; k < str.Length; ++k, ++curInd)
+                    {
+                        if (str[k] != ' ')
+                        {
+                            curStr += str[k];
+
+                            if ((curInd + 1) </*= InputArraySize */str.Length && str[k + 1] != ' ')
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                array.Add(Convert.ToInt32(curStr.ToString()));
+                                curStr = string.Empty;
+                            }
+                        }
+                    }
+                }
+                Sum(array);
+            }
+        }
+
+        private static void Sum(List<int> arrSum)
+        {
+            //List<int> arr = new List<int>() { 1, 2, 3, 3 };
             bool isContain = false;
 
-            for (int i = 1; i < arr.Count - 1; i++)
+            for (int i = 1; i < arrSum.Count - 1; i++)
             {
-                if (arr.GetRange(0, i).Sum() ==
-                    arr.GetRange(i + 1, arr.Count - i - 1).Sum())
+                if (arrSum.GetRange(0, i).Sum() ==
+                    arrSum.GetRange(i + 1, arrSum.Count - i - 1).Sum())
                 {
                     isContain = true;
                     break;
                 }
             }
 
-
             if (isContain)
             {
-                Console.WriteLine("Yes");
+                Console.WriteLine("YES");
             }
             else
             {
-                Console.WriteLine("No");
+                Console.WriteLine("NO");
             }
-
-        }
-
-        private static int Sum(List<int> arrSum)
-        {
-            return arrSum.Sum();
         }
 
 
