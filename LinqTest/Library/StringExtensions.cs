d04b0b1c19d714f854029848e258e46c84e9a1ee
Hayk Harutyunyan <hayk.haru@gmail.com>
2017-03-24 15:57:27 +0400

    Linq tests

---
+++ LinqTest/Library/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Threading;
+
+namespace Library
+{
+    public static class StringExtensions
+    {
+        public static string ConvertToTitleCase(this string source)
+        {
+            CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
+            TextInfo textInfo = cultureInfo.TextInfo;
+
+            return textInfo.ToTitleCase(source);
+        }
+    }
+}
