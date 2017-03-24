3e5c9b31b9ddc80b66436905e28b764ad3195401
Hayk Harutyunyan <hayk.haru@gmail.com>
2017-03-24 15:58:55 +0400

    Linq tests

---
+++ LinqTest/Test/LibraryTest.cs
@@ -0,0 +1,24 @@
+﻿using Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class StringExtensionsTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void ConvertToTitleCase()
+        {
+            var source = "the return of the king";
+            var expected = "The Return Of The King";
+
+            //var result = StringExtensions.ConvertToTitleCase(source);
+            var result = source.ConvertToTitleCase();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
--- LinqTest/Test/UnitTest1.cs
+++ LinqTest/Test/UnitTest1.cs
@@ -8,18 +8,27 @@ namespace Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
-        {
-
-        }
-
-        [TestMethod]
         public void find()
         {
             CustomerRepository repository = new CustomerRepository();
             var customerList = repository.Retrieve();
 
             var result = repository.Find(customerList, 2);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.CustomerId);
+            Assert.AreEqual("Baggins", result.LastName);
+            Assert.AreEqual("Bilbo", result.FirtsName);
+        }
+
+        [TestMethod]
+        public void FindTestNotFound()
+        {
+            CustomerRepository repository = new CustomerRepository();
+            var customerList = repository.Retrieve();
+
+            var result = repository.Find(customerList, 42);
+            Assert.IsNull(result);
         }
 
 
